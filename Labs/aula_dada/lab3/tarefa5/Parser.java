e8442623b95b4bf2d9c08a57f9b2d7a7f7df6380
Ingrid Santos <ils@cesar.org.br>
2019-07-12 09:44:07 -0400

    Laboratórios de aula prática

---
+++ Labs/aula_dada/lab3/tarefa5/Parser.java
@@ -0,0 +1,264 @@
+
+
+import java.io.*;
+import java.util.ArrayList;
+
+class HTMLSource
+{
+    private String source;
+    private int qid;
+
+    public HTMLSource() {
+        qid = 0;
+        source = "<html>\n<head><title>Formulario</title></head>\n<body>\n<form>";
+    };
+
+    public void addText(String str) {
+        source += str + "<br>\n";
+        source += "<textarea name='Q" + qid 
+               + "' cols='50' rows='3'></textarea><br>\n<br>";
+        qid ++;
+    }
+
+    public void addRadio(String str, ArrayList<String> opcoes) {
+        source += str + "<br>\n";
+        for (String opcao: opcoes) {
+            source += "<input type='radio' name='Q" + qid 
+                + "' value='" + opcao + "'>" + opcao + "<br>\n";
+        }
+        source += "<br>\n";
+        qid ++;
+    }
+
+    public void addCheckBox(String str, ArrayList<String> opcoes) {
+        source += str + "<br>\n";
+        for (String opcao: opcoes) {
+            source += "<input type='checkbox' name='Q" + qid 
+                + "' value='" + opcao + "'>" + opcao + "<br>\n";
+        }
+        source += "<br>\n";
+        qid ++;
+    }
+
+    public String finish() {
+        source += "</form>\n</body>\n</html>";
+        return source;
+    }
+}
+
+
+
+public class Parser {
+	public static final int _EOF = 0;
+	public static final int _string = 1;
+	public static final int maxT = 8;
+
+	static final boolean T = true;
+	static final boolean x = false;
+	static final int minErrDist = 2;
+
+	public Token t;    // last recognized token
+	public Token la;   // lookahead token
+	int errDist = minErrDist;
+	
+	public Scanner scanner;
+	public Errors errors;
+
+	HTMLSource html;
+
+
+
+	public Parser(Scanner scanner) {
+		this.scanner = scanner;
+		errors = new Errors();
+	}
+
+	void SynErr (int n) {
+		if (errDist >= minErrDist) errors.SynErr(la.line, la.col, n);
+		errDist = 0;
+	}
+
+	public void SemErr (String msg) {
+		if (errDist >= minErrDist) errors.SemErr(t.line, t.col, msg);
+		errDist = 0;
+	}
+	
+	void Get () {
+		for (;;) {
+			t = la;
+			la = scanner.Scan();
+			if (la.kind <= maxT) {
+				++errDist;
+				break;
+			}
+
+			la = t;
+		}
+	}
+	
+	void Expect (int n) {
+		if (la.kind==n) Get(); else { SynErr(n); }
+	}
+	
+	boolean StartOf (int s) {
+		return set[s][la.kind];
+	}
+	
+	void ExpectWeak (int n, int follow) {
+		if (la.kind == n) Get();
+		else {
+			SynErr(n);
+			while (!StartOf(follow)) Get();
+		}
+	}
+	
+	boolean WeakSeparator (int n, int syFol, int repFol) {
+		int kind = la.kind;
+		if (kind == n) { Get(); return true; }
+		else if (StartOf(repFol)) return false;
+		else {
+			SynErr(n);
+			while (!(set[syFol][kind] || set[repFol][kind] || set[0][kind])) {
+				Get();
+				kind = la.kind;
+			}
+			return StartOf(syFol);
+		}
+	}
+	
+	void Form() {
+		html = new HTMLSource(); 
+		Questao();
+		while (la.kind == 2 || la.kind == 3 || la.kind == 4) {
+			Questao();
+		}
+		System.out.println(html.finish()); 
+	}
+
+	void Questao() {
+		if (la.kind == 2) {
+			Texto();
+		} else if (la.kind == 3) {
+			EscolhaUma();
+		} else if (la.kind == 4) {
+			EscolhaVarias();
+		} else SynErr(9);
+	}
+
+	void Texto() {
+		Expect(2);
+		Expect(1);
+		html.addText(t.val.substring(1, t.val.length()-1)); 
+	}
+
+	void EscolhaUma() {
+		String str = ""; 
+		ArrayList<String> opcoes; 
+		Expect(3);
+		Expect(1);
+		str = t.val.substring(1, t.val.length()-1); 
+		opcoes = ListaOpcoes();
+		html.addRadio(str, opcoes); 
+	}
+
+	void EscolhaVarias() {
+		String str = ""; ArrayList<String> opcoes; 
+		Expect(4);
+		Expect(1);
+		str = t.val.substring(1, t.val.length()-1); 
+		opcoes = ListaOpcoes();
+		html.addCheckBox(str, opcoes); 
+	}
+
+	ArrayList<String>  ListaOpcoes() {
+		ArrayList<String>  opcoes;
+		opcoes = new ArrayList<String>(); 
+		Expect(5);
+		Expect(1);
+		opcoes.add(t.val.substring(1, t.val.length()-1)); 
+		while (la.kind == 6) {
+			Get();
+			Expect(1);
+			opcoes.add(t.val.substring(1, t.val.length()-1)); 
+		}
+		Expect(7);
+		return opcoes;
+	}
+
+
+
+	public void Parse() {
+		la = new Token();
+		la.val = "";		
+		Get();
+		Form();
+		Expect(0);
+
+	}
+
+	private static final boolean[][] set = {
+		{T,x,x,x, x,x,x,x, x,x}
+
+	};
+} // end Parser
+
+
+class Errors {
+	public int count = 0;                                    // number of errors detected
+	public java.io.PrintStream errorStream = System.out;     // error messages go to this stream
+	public String errMsgFormat = "-- line {0} col {1}: {2}"; // 0=line, 1=column, 2=text
+	
+	protected void printMsg(int line, int column, String msg) {
+		StringBuffer b = new StringBuffer(errMsgFormat);
+		int pos = b.indexOf("{0}");
+		if (pos >= 0) { b.delete(pos, pos+3); b.insert(pos, line); }
+		pos = b.indexOf("{1}");
+		if (pos >= 0) { b.delete(pos, pos+3); b.insert(pos, column); }
+		pos = b.indexOf("{2}");
+		if (pos >= 0) b.replace(pos, pos+3, msg);
+		errorStream.println(b.toString());
+	}
+	
+	public void SynErr (int line, int col, int n) {
+		String s;
+		switch (n) {
+			case 0: s = "EOF expected"; break;
+			case 1: s = "string expected"; break;
+			case 2: s = "\"TEXTO\" expected"; break;
+			case 3: s = "\"ESCOLHAUMA\" expected"; break;
+			case 4: s = "\"ESCOLHAVARIAS\" expected"; break;
+			case 5: s = "\"(\" expected"; break;
+			case 6: s = "\",\" expected"; break;
+			case 7: s = "\")\" expected"; break;
+			case 8: s = "??? expected"; break;
+			case 9: s = "invalid Questao"; break;
+			default: s = "error " + n; break;
+		}
+		printMsg(line, col, s);
+		count++;
+	}
+
+	public void SemErr (int line, int col, String s) {	
+		printMsg(line, col, s);
+		count++;
+	}
+	
+	public void SemErr (String s) {
+		errorStream.println(s);
+		count++;
+	}
+	
+	public void Warning (int line, int col, String s) {	
+		printMsg(line, col, s);
+	}
+	
+	public void Warning (String s) {
+		errorStream.println(s);
+	}
+} // Errors
+
+
+class FatalError extends RuntimeException {
+	public static final long serialVersionUID = 1L;
+	public FatalError(String s) { super(s); }
+}
